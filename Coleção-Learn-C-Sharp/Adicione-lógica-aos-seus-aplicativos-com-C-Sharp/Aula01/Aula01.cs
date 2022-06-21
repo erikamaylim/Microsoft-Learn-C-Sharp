9f2d4fd133f74e9530e79d328a85ebaeba7262c0
unknown <erikamayerlima@gmail.com>
2022-06-21 13:19:16 -0300

    atualização

---
+++ Coleção-Learn-C-Sharp/Adicione-lógica-aos-seus-aplicativos-com-C-Sharp/Aula01/Aula01.cs
@@ -0,0 +1,21 @@
+﻿// Expressões boolianas
+class Aula01
+{
+    static void Main()
+    {
+        Console.WriteLine($"a = a: {"a"}");
+        /* Console.WriteLine("a" == "a ");
+
+        Console.WriteLine("a" == "A");
+        Console.WriteLine(1 == 2);
+
+        string letra = "a";
+        Console.WriteLine(letra == "a");
+
+        // Moldar dados
+        string letra1 = " a";
+        string letra2 = "A ";
+        Console.WriteLine(letra1.Trim().ToLower() == letra2.Trim().ToLower());
+        Console.WriteLine(letra1.Trim().ToUpper() == letra2.Trim().ToUpper()); */
+    }
+}

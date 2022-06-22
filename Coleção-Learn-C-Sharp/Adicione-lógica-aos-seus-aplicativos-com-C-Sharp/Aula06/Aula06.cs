7ef7d8f5b280d2cd8d70a196b617813141006642
Erika <erikamayerlima@gmail.com>
2022-06-22 14:25:37 -0300

    novas aulas

---
+++ Coleção-Learn-C-Sharp/Adicione-lógica-aos-seus-aplicativos-com-C-Sharp/Aula06/Aula06.cs
@@ -0,0 +1,21 @@
+﻿// Blocos de código afetam o escopo de uma declaração de variáveis. Escopo de variável é a visibilidade da variável para outro código
+class Aula06
+{
+    static void Main()
+    {
+        bool flag = true;
+        int n1 = 0;
+
+        if (flag)
+        {
+            n1 = 2;
+            Console.WriteLine($"n1 dentro do bloco de código: {n1}");
+            int n2 = 10;
+            Console.WriteLine($"n2 dentro do bloco de código: {n2}");
+        }
+
+        Console.WriteLine($"n1 fora do bloco de código: {n1}");
+        Console.WriteLine($"n2 fora do bloco de código: {n2}");   // gera mensagem de erro
+
+    }
+}
